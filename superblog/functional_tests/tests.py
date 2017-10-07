203da1b1cc86b004a0c54600dc046b060cb88872
mgl <m.g.lee@cs.bham.ac.uk>
2017-10-07 15:07:51 +0100

    setting up basic ft directory

---
+++ superblog/functional_tests/tests.py
@@ -0,0 +1,17 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+import time
+import os, unittest
+
+class BlogTests (LiveServerTestCase):
+       def setUp(self):
+           self.browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
+
+       def tearDown (self):
+            self.browser.quit()
+
+       # Insert our user story here
+           
+        
